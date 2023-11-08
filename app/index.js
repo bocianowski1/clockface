4b8442d6ef12441810f8ddd7dd4d2a5150b746d6
torgerbocianowski <torgerboc@gmail.com>
2023-11-08 23:53:18 +0100

    place items

--- app/index.js
+++ app/index.js
@@ -1,6 +1,9 @@
 import clock from "clock";
 import document from "document";
-import { preferences } from "user-settings";
+// show battery level, steps, and heart rate
+import { battery, charger } from "power";
+import { HeartRateSensor } from "heart-rate";
+import { today } from "user-activity";
 
 function zeroPad(i) {
   if (i < 10) {
@@ -9,23 +12,64 @@ function zeroPad(i) {
   return i;
 }
 
-// Update the clock every minute
 clock.granularity = "minutes";
 
 // Get a handle on the <text> element
-const myLabel = document.getElementById("myLabel");
+const timeLabel = document.getElementById("timeLabel");
 
 // Update the <text> element every tick with the current time
 clock.ontick = (evt) => {
   let today = evt.date;
   let hours = today.getHours();
-  if (preferences.clockDisplay === "12h") {
-    // 12h format
-    hours = hours % 12 || 12;
-  } else {
-    // 24h format
-    hours = zeroPad(hours);
-  }
+
+  hours = zeroPad(hours);
   let mins = zeroPad(today.getMinutes());
-  myLabel.text = `${hours}:${mins}`;
+  timeLabel.text = `${hours}:${mins}`;
 };
+
+// show battery level
+const batteryLabel = document.getElementById("batteryLabel");
+batteryLabel.text = `${Math.floor(battery.chargeLevel)}%`;
+
+battery.onchange = () => {
+  batteryLabel.text = `${Math.floor(battery.chargeLevel)}%`;
+};
+
+// show date
+const dateLabel = document.getElementById("dateLabel");
+const months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Nov",
+  "Dec",
+];
+const todayDate = new Date();
+const month = months[todayDate.getMonth() - 1];
+const date = todayDate.getDate();
+dateLabel.text = `${date}. ${month}`;
+
+// show heart rate
+const hrm = new HeartRateSensor();
+hrm.start();
+const heartRateLabel = document.getElementById("heartRateLabel");
+hrm.onreading = () => {
+  heartRateLabel.text = `${hrm.heartRate}`;
+};
+
+// show steps
+const stepsLabel = document.getElementById("stepsLabel");
+stepsLabel.text = `${today.local.steps}`;
+
+today.local.onchange = () => {
+  stepsLabel.text = `${today.local.steps}`;
+};
+
+const divider = document.getElementById("divider");
+divider.text = "|";
